a65756c61c0a24d76e16e85631f372251784356e
Alex Pham <alexanderpham023@gmail.com>
2024-09-24 13:37:55 -0700

    take 2: not for the 3 commits, setting up basic pages/routing to work on

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomePageComponent } from './Pages/Homepage/home-page/home-page.component';
+import { LoginPageComponent } from './Pages/Loginpage/login-page/login-page.component';
+export const routes: Routes = [
+    {path: '', component:HomePageComponent},
+    {path: 'login', component:LoginPageComponent}
+];

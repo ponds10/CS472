e40777cf29c172d68f1476ac6f4a3ae2581a8428
Alex Pham <alexanderpham023@gmail.com>
2024-09-26 23:46:43 -0700

    More project filesystem set up, adding shared components. Set up login page and routing from home page.

--- frontend/src/app/Pages/Homepage/home-page/home-page.component.spec.ts
+++ frontend/src/app/Pages/Homepage/home-page/home-page.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HeaderComponent } from '../../../../shared/header/header.component';
 import { HomePageComponent } from './home-page.component';
 
 describe('HomePageComponent', () => {
@@ -8,7 +8,7 @@ describe('HomePageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomePageComponent]
+      imports: [HomePageComponent, HeaderComponent]
     })
     .compileComponents();
     
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html'
 })
 export class AppComponent {

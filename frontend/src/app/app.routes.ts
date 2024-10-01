eb954b5b19f692e13b3512d055de1f0a8e1edcda
Alex Pham <alexanderpham023@gmail.com>
2024-10-01 14:01:49 -0700

    page set up/routing again

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,16 @@
 import { Routes } from '@angular/router';
-import { LandingPageComponent } from './pages/landing-page/landing-page.component'
-import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { LandingPageComponent } from './Pages/landing-page/landing-page.component'
+import { LoginPageComponent } from './Pages/login-page/login-page.component';
+import { EventPageComponent } from './Pages/event-page/event-page.component';
+import { PetPageComponent } from './Pages/pet-page/pet-page.component';
+import { HomePageComponent } from './Pages/home-page/home-page.component';
+import { DocumentsPageComponent } from './Pages/documents-page/documents-page.component';
+
 export const routes: Routes = [
     {path: '', component: LandingPageComponent},
     {path: 'login', component: LoginPageComponent},
+    {path: 'eventpage', component: EventPageComponent},
+    {path: 'petpage', component: PetPageComponent},
+    {path: 'homepage', component: HomePageComponent},
+    {path: 'documentspage', component: DocumentsPageComponent}
 ];

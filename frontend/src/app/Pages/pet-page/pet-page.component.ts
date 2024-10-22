c7fb2d712f31d60a36c315a5e67ba925e3761902
CoMoMi <97932512+CoMoMi@users.noreply.github.com>
2024-10-22 12:19:50 -0700

    changed pet page to pull info from a source, also added pet list page

--- frontend/src/app/Pages/pet-page/pet-page.component.ts
+++ frontend/src/app/Pages/pet-page/pet-page.component.ts
@@ -1,13 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { NavBarComponent } from '../../../shared/nav-bar/nav-bar.component';
+import { Pet } from '../../pet.model';
+import { CommonModule } from '@angular/common';
+import { PetService } from '../../pet.service';
 
 @Component({
   selector: 'app-pet-page',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [NavBarComponent, CommonModule],
   templateUrl: './pet-page.component.html',
-  styleUrl: './pet-page.component.css'
+  styleUrl: './pet-page.component.css',
 })
-export class PetPageComponent {
+export class PetPageComponent implements OnInit {
+  pet?: Pet;
 
+  isLoading: boolean = true;
+  errorMessage: string = '';
+
+  constructor(private route: ActivatedRoute, private petService: PetService) { }
+
+  ngOnInit(): void {
+    const idParam = this.route.snapshot.paramMap.get('id');
+    const id = idParam ? Number(idParam) : null;
+
+    if (id) {
+      this.petService.getPetById(id).subscribe({
+        next: (data: Pet | undefined) => {
+          if (data) {
+            this.pet = data;
+          } else {
+            this.errorMessage = 'Pet not found.';
+          }
+          this.isLoading = false;
+        },
+        error: (error) => {
+          console.error('Error fetching pet details:', error);
+          this.errorMessage = 'Unable to load pet details. Please try again later.';
+          this.isLoading = false;
+        }
+      });
+    } else {
+      this.errorMessage = 'Invalid pet ID.';
+      this.isLoading = false;
+    }
+
+  }
+
+  formatPhone(phone: string): string {
+    return phone.replace(/[^0-9+]/g, '');
+  }
 }
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { RouterLink } from '@angular/router';
 import { RouterLinkActive } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { PetListComponent } from './Pages/pet-list/pet-list.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, ReactiveFormsModule],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, ReactiveFormsModule, PetListComponent],
   templateUrl: './app.component.html'
 })
 export class AppComponent {

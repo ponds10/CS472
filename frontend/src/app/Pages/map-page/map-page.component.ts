cb57a7731123b7894fe2d3dd025b8146c56622dc
Alistair <alistairnds@gmail.com>
2024-10-22 13:56:13 -0700

    Update map page with formatting

--- frontend/src/app/Pages/map-page/map-page.component.ts
+++ frontend/src/app/Pages/map-page/map-page.component.ts
@@ -1,12 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { HeaderComponent } from '../../../shared/header/header.component';
+import { NavBarComponent } from '../../../shared/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-map-page',
   standalone: true,
   imports: [
     CommonModule,
-  ],
+    HeaderComponent,
+    NavBarComponent
+],
   templateUrl: './map-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })

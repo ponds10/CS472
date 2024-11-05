b40e9b5df77de4d475a03345e651bb7bfc5df0d7
Alistair Delos Santos <alistairnds@gmail.com>
2024-11-05 11:54:36 -0800

    Fix providers config

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -9,6 +9,6 @@ import { getStorage, provideStorage } from '@angular/fire/storage';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"toebeans-d6f03","appId":"1:10600296290:web:ccff98c6130c4bb517c63b","storageBucket":"toebeans-d6f03.appspot.com","locationId":"us-central","apiKey":"AIzaSyADxlQyq1u7g4LULCI9RfhKdheVkAOscoI","authDomain":"toebeans-d6f03.firebaseapp.com","messagingSenderId":"10600296290","measurementId":"G-J0JRTK8PVE"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage()) // Provide routing globally
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"toebeans-d6f03","appId":"1:10600296290:web:ccff98c6130c4bb517c63b","storageBucket":"toebeans-d6f03.appspot.com","apiKey":"AIzaSyADxlQyq1u7g4LULCI9RfhKdheVkAOscoI","authDomain":"toebeans-d6f03.firebaseapp.com","messagingSenderId":"10600296290","measurementId":"G-J0JRTK8PVE"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage()) // Provide routing globally
   ],
 }).catch((err) => console.error(err));

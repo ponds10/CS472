4dce6a335c05ec3526a8a860b950f2af12ef2865
Alistair Delos Santos <alistairnds@gmail.com>
2024-11-05 11:50:55 -0800

    Create environment file for Firebase

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyADxlQyq1u7g4LULCI9RfhKdheVkAOscoI",
+    authDomain: "toebeans-d6f03.firebaseapp.com",
+    projectId: "toebeans-d6f03",
+    storageBucket: "toebeans-d6f03.appspot.com",
+    messagingSenderId: "10600296290",
+    appId: "1:10600296290:web:ccff98c6130c4bb517c63b",
+    measurementId: "G-J0JRTK8PVE"
+  }
+}

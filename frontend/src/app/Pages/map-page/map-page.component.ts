0287ab51320ba987d0f7cab696f4d116050f2fbc
Alistair Delos Santos <alistairnds@gmail.com>
2024-11-07 11:54:36 -0800

    Add new marker button to map (not functional yet)

--- frontend/src/app/Pages/map-page/map-page.component.ts
+++ frontend/src/app/Pages/map-page/map-page.component.ts
@@ -21,7 +21,10 @@ import { MapInfoBoxComponent } from "./map-info-box/map-info-box.component";
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MapPageComponent { 
-  @ViewChild(MapInfoWindow) infoWindow!: MapInfoWindow;
+  
+
+  @ViewChild(MapInfoWindow) infoWindow!: MapInfoWindow
+  @ViewChild(GoogleMap) map!: GoogleMap
 
   center: google.maps.LatLngLiteral = {lat: 36.180545979079874, lng: -115.17917779168842}
   zoom = 11
@@ -32,4 +35,39 @@ export class MapPageComponent {
     this.infoWindow.open(marker)
   }
 
+  createNewMarkerControl(map: GoogleMap) {
+    const controlButton = document.createElement('button')
+
+    controlButton.style.backgroundColor = '#fff';
+    controlButton.style.border = '2px solid #fff';
+    controlButton.style.borderRadius = '3px';
+    controlButton.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+    controlButton.style.color = 'rgb(25,25,25)';
+    controlButton.style.cursor = 'pointer';
+    controlButton.style.fontFamily = 'Roboto,Arial,sans-serif';
+    controlButton.style.fontSize = '16px';
+    controlButton.style.lineHeight = '38px';
+    controlButton.style.margin = '8px 0 22px';
+    controlButton.style.padding = '0 5px';
+    controlButton.style.textAlign = 'center';
+
+    controlButton.textContent = 'New Marker';
+    controlButton.title = 'Click to create new marker';
+    controlButton.type = 'button';
+
+    controlButton.addEventListener('click', () => {
+      console.log('Button works!')
+    });
+
+    return controlButton
+  }
+
+  
+
+  addControl() {
+    const newMarkerControl = this.createNewMarkerControl(this.map)
+    const newMarkerControlDiv = document.createElement('div')
+    newMarkerControlDiv.appendChild(newMarkerControl)
+    this.map.controls[google.maps.ControlPosition.TOP_CENTER].push(newMarkerControlDiv)
+  }
 }

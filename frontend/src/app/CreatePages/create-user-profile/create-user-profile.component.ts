911d6ce10469b846012931edbb3b1464659c0afd
Alex Pham <alexanderpham023@gmail.com>
2024-11-07 00:37:37 -0800

    skeleton stuff for when a user is created/logging in for the first time w/reactive forms

--- frontend/src/app/CreatePages/create-user-profile/create-user-profile.component.ts
+++ frontend/src/app/CreatePages/create-user-profile/create-user-profile.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
-
+import { HeaderComponent } from '../../../shared/header/header.component';
+import { MatIcon } from '@angular/material/icon';
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-create-user-profile',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, MatIcon, ReactiveFormsModule],
   templateUrl: './create-user-profile.component.html',
-  styleUrl: './create-user-profile.component.css'
 })
 export class CreateUserProfileComponent {
+  steps: number[] = [1, 0, 0, 0, 0]
+  current_step: number = 0;
+  max: number = 0;
 
+  fg_basic_info = new FormGroup({
+    fname: new FormControl(''),
+    lname: new FormControl(''),
+  });
+
+
+  next()
+  {
+    if(this.current_step + 1 == this.max){return;}
+
+    this.steps[this.current_step]=0;
+    this.current_step++;
+    this.steps[this.current_step]=1
+  }
+  back()
+  {
+    if(this.current_step == 0){return;}
+    this.steps[this.current_step]=0;
+    this.current_step--;
+    this.steps[this.current_step]=1
+  }
 }

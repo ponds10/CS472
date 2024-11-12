31e349c6bc9cbd195eeceeae74c2b19c0b4b6442
Chalinee Karawek <chalineekarawek@gmail.com>
2024-11-11 22:10:48 -0800

    Added 3 case study buttons

--- frontend/src/app/Pages/landing-page/landing-page.component.ts
+++ frontend/src/app/Pages/landing-page/landing-page.component.ts
@@ -13,16 +13,24 @@ import { FooterComponent } from '../../../shared/footer/footer.component';
     LoginBoxComponent,
     HeaderComponent,
     FooterComponent
-],
- /*  template: `
-    @if(){
-
-    }
-  `, */
+  ],
   templateUrl: './landing-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LandingPageComponent { 
-  constructor(public navService: NavigationServiceService){}
+
+export class LandingPageComponent {
+  // control the visibility of the descriptions
+  showDescription1 = false;
+  showDescription2 = false;
+  showDescription3 = false;
+
+  constructor(public navService: NavigationServiceService) {}
+
+  // toggle description visibility
+  toggleDescription(container: number) {
+    if (container === 1) this.showDescription1 = !this.showDescription1;
+    else if (container === 2) this.showDescription2 = !this.showDescription2;
+    else if (container === 3) this.showDescription3 = !this.showDescription3;
+  }
 }
 

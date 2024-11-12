e65826c5d89038dde7fbd764503b54d545da55c0
Alistair Delos Santos <alistairnds@gmail.com>
2024-11-12 15:36:43 -0800

    fixed map mobile formatting

--- frontend/src/app/Pages/map-page/map-info-box/map-info-box.component.ts
+++ frontend/src/app/Pages/map-page/map-info-box/map-info-box.component.ts
@@ -6,16 +6,14 @@ import { MatDialogRef } from '@angular/material/dialog';
 @Component({
   selector: 'app-map-info-box',
   standalone: true,
-  imports: [
-    CommonModule, MatIcon
-  ],
+  imports: [CommonModule, MatIcon],
   templateUrl: './map-info-box.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MapInfoBoxComponent { 
-  constructor(public dialogRef: MatDialogRef<MapInfoBoxComponent>) { }
+export class MapInfoBoxComponent {
+  constructor(public dialogRef: MatDialogRef<MapInfoBoxComponent>) {}
 
   closeDialog(): void {
-    this.dialogRef.close('hi')
+    this.dialogRef.close('hi');
   }
 }

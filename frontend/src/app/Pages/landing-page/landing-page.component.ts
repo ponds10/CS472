fb2268606a249faa0c5c812eb5c69d16c7cab672
Alex Pham <alexanderpham023@gmail.com>
2024-11-14 10:13:43 -0800

    quick fix: landing page formatting for case studies section

--- frontend/src/app/Pages/landing-page/landing-page.component.ts
+++ frontend/src/app/Pages/landing-page/landing-page.component.ts
@@ -20,17 +20,33 @@ import { FooterComponent } from '../../../shared/footer/footer.component';
 
 export class LandingPageComponent {
   // control the visibility of the descriptions
-  showDescription1 = false;
+  showDescription1 = true;
   showDescription2 = false;
   showDescription3 = false;
+
+  description1: string = "desc 1";
+  description2: string = "desc 2";
+  description3: string = "desc 3";
 
   constructor(public navService: NavigationServiceService) {}
 
   // toggle description visibility
   toggleDescription(container: number) {
-    if (container === 1) this.showDescription1 = !this.showDescription1;
-    else if (container === 2) this.showDescription2 = !this.showDescription2;
-    else if (container === 3) this.showDescription3 = !this.showDescription3;
+    if(container == 1)
+    {
+      this.showDescription1 = true;
+      this.showDescription2 = this.showDescription3 = false;
+    }
+    else if(container == 2)
+    {
+      this.showDescription2 = true;
+      this.showDescription1 = this.showDescription3 = false;
+    }
+    else if(container == 3)
+    {
+      this.showDescription3 = true;
+      this.showDescription1 = this.showDescription2 = false;
+    }
   }
 }
 
